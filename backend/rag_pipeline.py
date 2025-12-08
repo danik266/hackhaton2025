3e0fe19bf2b87a2298f173e2accd704a780d1b04
danik266 <aldiyar_08@bk.ru>
2025-12-08 22:50:31 +0500

    dsa

--- backend/rag_pipeline.py
+++ backend/rag_pipeline.py
@@ -2,7 +2,7 @@ import csv
 import os
 from .gemini_client import GeminiClient
 
-YOUR_GEMINI_API_KEY = "AIzaSyDZpyKnNfpspg6ECVNFRCrYVY9Pmtsu3Ko"
+YOUR_GEMINI_API_KEY = "AIzaSyDqM-1Q-9W1OjR_RjQzqljca1c_HpBsWMw"
 gemini = GeminiClient(api_key=YOUR_GEMINI_API_KEY)
 
 def load_knowledge(filename="data/drugs.csv"):
